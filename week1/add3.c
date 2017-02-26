df277074f411c34856e344c19888869c60ee3970
oromis13 <viba16cs@cmrit.ac.in>
2017-02-26 12:23:06 +0530

    adding 3 numbers
    removes i/o from compute

--- week1/add3.c
+++ week1/add3.c
@@ -2,22 +2,22 @@
 int main()
 {
 
-int a,b,c;
+int a,b,c,sum;
 
 printf("input three numbers to be added ");
 
 scanf("%d%d%d",&a,&b,&c);
 
-add(a,b,c);
+sum=add(a,b,c);
+printf(" sum is %d",sum);
 
 return (0);
 
 }
 
 
-void add( int a, int b,int c)
+int add( int a, int b,int c)
 {
- int sum=a+b+c;
+ return(a+b+c);
  
- printf(" sum is %d",sum);
  }
